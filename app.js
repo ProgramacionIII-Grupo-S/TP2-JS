bfe505790f8596b139428115ff1eb1cc39f28734
Yamila Aguilar <aguilaryamila995@gmail.com>
2025-08-21 00:42:58 -0300

    Se agrego los puntos del TP: agregar, eliminar y editar un producto

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ async function getProductosLimitados(limit = 5) {
     return await fetchProductos(`${API_URL}?limit=${limit}`);
 }
 
+
 // Guardar datos en JSON
 function guardarEnJSON(filename, data) {
     try {
@@ -36,6 +37,71 @@ function guardarEnJSON(filename, data) {
     }
 }
 
+// Agregar un nuevo producto con el metodo POST
+
+async function agregarProducto() {
+    const nuevoProducto = {
+        title: "Par de medias",
+        price: 2.5,
+        description: "Medias de algodon grueso, largas y de color negro",
+        image: "https://acdn-us.mitiendanube.com/stores/001/709/280/products/d1-57c4c861e5f616bcef16249705290487-1024-1024.webp",
+        category: "men's clothing"
+    };
+
+    try {
+        const res = await fetch(API_URL, {
+            method: "POST",
+            body: JSON.stringify(nuevoProducto),
+            headers: { "Content-Type": "application/json" }
+        });
+        const data = await res.json();
+        console.log("Producto agregado:", data);
+
+    } catch (error) {
+        console.error("Error al agregar producto:", error);
+    }
+}
+
+//Eliminar un producto
+
+async function borrarProducto(id) {
+    try {
+        const res = await fetch(`${API_URL}/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        const data = await res.json();
+        console.log(`Producto eliminado:`, data);
+    } catch (error) {
+        console.error('Error al borrar el producto:', error);
+    }
+}
+
+//Modificar un producto
+
+async function editarProducto(id) {
+    const productoModificado = {
+        title: "Guantes para la moto",
+        price: 5.0,
+        description: "Guantes de cuero con aislamiento termico para el invierno",
+        image: "https://www.repsol.es/content/dam/repsol-ecommerce/tienda/productos/g/guantes-para-moto-alpinestar-mm93-rio-hondo-v2-air/guantes-para-moto-alpinestar-mm93-rio-hondo-v2-air-001.jpg",
+        category: "men's clothing"
+    };
+
+    try {
+        const res = await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(productoModificado),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const data = await res.json();
+        console.log(`Producto modificado:`, data);
+    } catch (error) {
+        console.error("Error al modificar producto:", error);
+    }
+}
 
 //Buscar la información de un determinado producto, utilizando un “id” como parámetro (GET)
 async function getProductById(id) {
@@ -64,6 +130,7 @@ function preguntarId() {
     });
 }
 
+
 // Función principal
 async function main() {
     console.log('--- Recuperando todos los productos ---');
@@ -83,6 +150,18 @@ async function main() {
         guardarEnJSON('productos.json', productosLimitados);
         console.log(productosLimitados);
     }
+
+    console.log();
+    console.log('Se agrego un nuevo producto');
+    await agregarProducto();
+
+    console.log();
+    console.log('Se elimino el producto 19');
+    await borrarProducto(19);
+
+    console.log();
+    console.log('Se modifico el producto 15');
+    await editarProducto(15);
 
     // ----- Opción 1: ID definido en el código -----
     const idCodigo = 3; // Cambiar aquí para probar otro ID
